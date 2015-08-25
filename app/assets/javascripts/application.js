4f28fda01d69d1d9dd4de7b66aa75dec8df700bf
Alex Kardos <alexander.kardos@gmail.com>
2015-08-25 09:02:30 -0600

    Reproduced foundation topnav doesn't close on target blank.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require vendor/modernizr
+//= require foundation
 //= require_tree .
+
+$(function(){ $(document).foundation(); });
